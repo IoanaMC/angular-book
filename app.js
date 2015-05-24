96ae5c0f27ce3dac0ea4942ab1cb7e2b5525231b
IoanaMC <yoanna90ro@yahoo.com>
2015-05-24 14:45:24 +0300

    Filters - last exercise.

--- app.js
+++ app.js
@@ -100,3 +100,127 @@ var app = angular.module("MyApp", []);
 // anotherCtrl.$inject = ["$scope", "UserService"];
 
 /* 12. Changing the DOM in Response to User Actions */
+
+ /* 19. Repeatedly Rendering Directive’s DOM Node Children */
+ // app.directive("repeatNtimes", function() {
+	//  return {
+	// 	 restrict: "E",
+	// 	 compile: function(element, attributes) {
+	// 		 var content = element.children();
+	// 		 for (var i=1; i<attributes.repeat; i++) {
+	// 		 	element.append(content.clone());
+	// 		 }
+	// 	 }
+	//  };
+ // });
+
+//v2 - nu merge
+ // app.directive("repeatNtimes", function() {
+	//  return {
+	// 	restrict: "E",
+	// 	compile: function(tElement, attrs) {
+	// 		var content = tElement.children();
+	// 		for (var i=1; i<attrs.repeat; i++) {
+	// 			tElement.append(content.clone());
+	// 		}
+
+	// 		return function (scope, element, attrs) {
+	// 		 element.on("click", "h1", function() {
+	// 		 	$(this).css({ "background-color": "red" });
+	// 		 });
+	// 		};
+	// 	 }
+	// }
+ // });
+
+ /* 20. Directive-to-Directive Communication */
+ // app.directive("basket", function() {
+	//  return {
+	// 	 restrict: "E",
+	// 	 controller: function($scope, $element, $attrs) {
+	// 		 $scope.content = [];
+
+	// 		 this.addApple = function() {
+	// 		 	$scope.content.push("apple");
+	// 		 };
+
+	// 		 this.addOrange = function() {
+	// 		 	$scope.content.push("orange");
+	// 		 };
+	// 	 },
+	// 	 link: function(scope, element) {
+	// 		element.bind("mouseenter", function() {
+	// 		 	console.log(scope.content);
+	// 	 	});
+	// 	 }
+	//  };
+ // });
+
+ // app.directive("apple", function() {
+	//  return {
+	// 	 require: "basket",
+	// 	 link: function(scope, element, attrs, basketCtrl) {
+	// 	 	basketCtrl.addApple();
+	// 	 }
+	//  };
+ // });
+
+ // app.directive("orange", function() {
+	//  return {
+	// 	 require: "basket",
+	// 	 link: function(scope, element, attrs, basketCtrl) {
+	// 	 	basketCtrl.addOrange();
+	// 	 }
+	//  };
+ // });
+
+ /* 22. Implementing a Custom Filter to Reverse an Input String */
+ // app.filter("reverse", function() {
+	//  return function(input) {
+	// 	 var result = "";
+	// 	 input = input || "";
+	// 	 for (var i=0; i<input.length; i++) {
+	// 	 	result = input.charAt(i) + result;
+	// 	 }
+	// 	 return result;
+	//  };
+ // });
+
+/* 23. Passing Configuration Params to Filters */
+// app.filter("reverse", function() {
+//  return function(input, options) {
+// 	 input = input || "";
+// 	 var result = "";
+// 	 var suffix = options["suffix"] || "";
+
+// 	 for (var i=0; i<input.length; i++) {
+// 	 	result = input.charAt(i) + result;
+// 	 }
+
+// 	 if (input.length > 0) result += suffix;
+
+// 	 return result;
+//  };
+// });
+
+/* 24.25. Filtering a List of DOM Nodes */
+app.filter("exclude", function() {
+ return function(input, exclude) {
+	 var result = [];
+	 for (var i=0; i<input.length; i++) {
+		 if (input[i] !== exclude) {
+		 	result.push(input[i]);
+		 }
+	 }
+
+	 return result;
+ };
+});
+
+/* 25. Chaining Filters together */
+app.filter("sortAscending", function() {
+ return function(input) {
+ }
+
+ return result;
+});
