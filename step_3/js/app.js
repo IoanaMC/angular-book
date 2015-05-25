6aea663960c5742514a850264b58a63f25a87ee0
IoanaMC <yoanna90ro@yahoo.com>
2015-05-25 21:22:06 +0300

    Angular Book - recipe.

---
+++ step_3/js/app.js
@@ -0,0 +1,17 @@
+/* 30. Using Route Location to Implement a Navigation Menu */
+var app = angular.module("MyApp", [])
+
+    .config(function($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.when("/home", {templateUrl: "partials/home.html", controller: "MainCtrl"})
+            .when("/help", {templateUrl: "partials/help.html", controller: "MainCtrl"})
+                .otherwise({redirectTo: "/home"});
+    })
+
+    .controller("MainCtrl", function($scope, $location) {
+        // console.log($location.path().substring(3));
+        $scope.menuClass = function(page) {
+            var current = $location.path().substring(1);
+            return page === current ? "active" : "";
+        };
+    })
